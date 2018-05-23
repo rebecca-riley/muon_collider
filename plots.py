272202e2a96b87c25cab0a26a987cf151104967a
rebecca-riley <rriley1@uci.edu>
2018-05-23 15:20:00 -0700

    Factorized IO. Refinedplotting pre-functions.

--- plots.py
+++ plots.py
@@ -5,29 +5,7 @@ num_bins = 150
 k=0
 
 def main():
-    file_input = input('Enter event files to plot: ').split()
-
-    file_read = []
-    events = []
-    for file in file_input:
-        try:
-            f = open(file,'r')
-            file_read.append(f)
-        except IOError:
-            print(file + ' not found. Try again with corrected input.')
-            return
-
-    for file in file_read:
-        events.append(cuts.processEvents(file))
-
-    if len(events) == 0:
-        print('No events read')
-        return
-    else:
-        total_events = 0
-        for event in events:
-            total_events += len(event)
-        print(str(total_events) + ' events read.')
+    file_data = fileReadIn()
 
     plots = input('Enter desired plots. Type \'help\' for options or \'quit\' to ' +
                   'exit. ').split()
@@ -50,47 +28,78 @@ def main():
         plots = input('Enter parameter to plot: ').split()
 
     if 'massfin' in plots:
-        plotFinalInvariantMass(events)
+        plotFinalInvariantMass(file_data)
     if 'massinit' in plots:
-        plotInitialInvariantMass(events)
+        plotInitialInvariantMass(file_data)
     if 'massfinpar' in plots:
         particle_list = input('Enter particle codes for partial invariant mass: ').split()
-        plotFinalInvariantMass(events, particle_list)
+        plotFinalInvariantMass(file_data, particle_list)
     if 'massinitpar' in plots:
         particle_list = input('Enter particle codes for partial invariant mass: ').split()
-        plotInitialInvariantMass(events, particle_list)
+        plotInitialInvariantMass(file_data, particle_list)
     if 'aglfin' in plots:
         particle_list = input('Enter two particle codes for angle between: ').split()
-        plotFinalAngle(events, particle_list)
+        plotFinalAngle(file_data, particle_list)
     if 'aglinit' in plots:
         particle_list = input('Enter two particle codes for angle between: ').split()
-        plotInitialAngle(events, particle_list)
+        plotInitialAngle(file_data, particle_list)
 
     return
 
+def fileReadIn():
+    file_input = input('Enter event files to plot: ').split()   #event files must be
+                                                    #entered by their full path name
+    if 'quit' in file_input:            #quit if user wants to exit
+        quit()
 
-def plotFinalInvariantMass(events,particle_list=[]):
-    plot(extractInvariantMass(events,cuts.final_state,particle_list),'Final invariant mass histogram','Invariant mass')
+    file_read = []                      #tries to read each file; allows user to try
+    file_data = []                         #again if name is incorrect
+    for file in file_input:
+        try:
+            f = open(file,'r')
+            file_read.append(f)
+        except IOError:
+            print(file + ' not found. Try again with corrected input.')
+            return fileReadIn()
 
-def plotInitialInvariantMass(events,particle_list=[]):
-    plot(extractInvariantMass(events,cuts.initial_state,particle_list),'Initial invariant mass histogram','Invariant mass')
+    for file in file_read:              #read in events from each file
+        print('Processing ' + file.name + '...')
+        file_data.append(cuts.processEvents(file))
 
-def plotFinalAngle(events,particle_list):
-    plot(extractAngle(events,cuts.final_state,particle_list),'Histogram of angle between '+particle_list[0]+' and '+particle_list[1],'Angle')
+    if len(file_data) == 0:                #report number of events read
+        print('No events read')
+        return
+    else:
+        total_events = 0
+        for event in file_data:
+            total_events += len(event)
+        print(str(total_events) + ' events read.')    
 
-def plotInitialAngle(events,particle_list):
-    plot(extractAngle(events,cuts.initial_state,particle_list),'Histogram of angle between '+particle_list[0]+' and '+particle_list[1],'Angle')
+    return file_data
 
-def extractInvariantMass(events,state,particle_list=[]):
-    return extractEvent(cuts.getInvariantMass,events,state,particle_list)
 
-def extractAngle(events,state,particle_list):
-    return extractEvent(cuts.getAngle,events,state,particle_list)
+def plotFinalInvariantMass(file_data,particle_list=[]):
+    plot(getDataToPlot(file_data,cuts.getInvariantMass,cuts.final_state,particle_list),'Final invariant mass histogram','Invariant mass')
 
-def extractEvent(fctn,events,state,particle_list):
+def plotInitialInvariantMass(file_data,particle_list=[]):
+    plot(getDataToPlot(file_data,cuts.getInvariantMass,cuts.initial_state,particle_list),'Initial invariant mass histogram','Invariant mass')
+
+def plotFinalAngle(file_data,particle_list):
+    plot(getDataToPlot(file_data,cuts.getAngle,cuts.final_state,particle_list),'Histogram of angle between '+particle_list[0]+' and '+particle_list[1],'Angle')
+
+def plotInitialAngle(file_data,particle_list):
+    plot(getDataToPlot(file_data,cuts.getAngle,cuts.initial_state,particle_list),'Histogram of angle between '+particle_list[0]+' and '+particle_list[1],'Angle')
+
+def getDataToPlot(file_data,fctn,state,particle_list):
+    data_to_plot = []
+    for file_events in file_data:
+        data_to_plot.append(extractEvent(file_events,fctn,state,particle_list))
+    return data_to_plot
+
+def extractEvent(file_data,fctn,state,particle_list):
     _event = []
     temp = []
-    for event in events:
+    for event in file_data:
         event_data = cuts.getEventData(event)  #retrieve relevant data
         if fctn == cuts.getAngle:
             temp.append(fctn(event_data,state,particle_list[0],particle_list[1]))
@@ -106,7 +115,10 @@ def extractEvent(fctn,events,state,particle_list):
 def plot(data,title,x_label):
     colors = ['b','g','r','o','y']
 
+    print(len(data))
     for i in range(len(data)):
+        print(i)
+        print(colors[i])
         plt.hist(data[i], num_bins, facecolor=colors[i], alpha = 0.5)      
 
     plt.xlabel(x_label)
