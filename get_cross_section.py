91d6e4d43fd3a6c484517854f62f2c0a4db7b254
rebecca-riley <rriley1@uci.edu>
2018-06-05 16:18:32 -0700

    gets cross section for all files current generated

--- get_cross_section.py
+++ get_cross_section.py
@@ -1,5 +1,9 @@
 # list of files to get cross section for
-filenames = ['ee_high_250.lhe','ee_low_500.lhe']
+filenames = ['ee_z_low_500.lhe','ee_sm_250.lhe','ee_sm_500.lhe','ee_small_250.lhe',
+             'ee_low_250.lhe','ee_low_500.lhe','ee_xa_sm_250.lhe','ee_xa_low_250.lhe',
+             'mu_a_low_6500.lhe','mu_h_low_6500.lhe','mu_sm_1000.lhe','mu_sm_10000.lhe',
+             'mu_low_6500.lhe','mu_z_low_6500.lhe','mu_sm_500.lhe','mu_low_500.lhe',
+             'mu_xa_sm_250.lhe']
 
 for file in filenames:
     try:                                #try to open file with inputted filename
@@ -11,4 +15,4 @@ for file in filenames:
         event_file.close()
     except IOError:                     #give error message, exit if file not found
         print(file + ' not found. Check list in script.')
-        
+
